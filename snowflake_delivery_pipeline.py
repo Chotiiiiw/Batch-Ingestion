6d6ba478417e4b80c9dd7bc74c56fa88f52cb136
Chotiwat <Chotiwatbanjongoru@gmail.com>
2026-07-28 20:43:18 +0700

    feat: add incremental loading for deliveries

--- snowflake_delivery_pipeline.py
+++ snowflake_delivery_pipeline.py
@@ -1,6 +1,6 @@
-from load.snowflake_common import complete_batch, fail_batch, start_batch
+from load.snowflake_common import complete_batch, fail_batch, get_last_mysql_watermark, start_batch
 from load.snowflake_delivery import merge_deliveries, stage_deliveries
-from transform.common import create_spark, read_mysql_table
+from transform.common import create_spark, get_mysql_watermark_to, read_mysql_incremental_table, read_mysql_table
 from transform.delivery import transform_deliveries
 
 
@@ -9,9 +9,22 @@ def main():
     batch_id = None
 
     try:
-        deliveries = read_mysql_table(
+        mysql_watermark_from = get_last_mysql_watermark(
+            "delivery",
+        )
+
+        mysql_watermark_to = get_mysql_watermark_to(
             spark,
             "deliveries",
+            "updated_at",
+        )
+
+        deliveries = read_mysql_incremental_table(
+            spark,
+            "deliveries",
+            "updated_at",
+            mysql_watermark_from,
+            mysql_watermark_to,
         )
 
         orders = read_mysql_table(
@@ -29,7 +42,7 @@ def main():
         staged_row_count = warehouse_deliveries.count()
         rejected_row_count = rejected_deliveries.count()
 
-        batch_id = start_batch("delivery", input_row_count, rejected_row_count)
+        batch_id = start_batch("delivery", input_row_count, rejected_row_count, mysql_watermark_from, mysql_watermark_to)
 
         stage_deliveries(
             warehouse_deliveries,
